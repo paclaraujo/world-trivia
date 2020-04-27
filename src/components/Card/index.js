0987dfda5c48f6a04fb684720683bf18c939c31a
paclaraujo <paclaraujo@gmail.com>
2020-04-26 21:37:51 -0300

    positioning card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,13 +4,13 @@ import './style.css';
 const Card = () => {
   return (
     <section className='box'>
-      <h1>How well do you know the world?</h1>
+      <h1 className="card-title">How well do you know the world?</h1>
       <div>
 
       </div>
       <p class='contacts'>Developed by 
         <a href='https://github.com/paclaraujo'>@paclaraujo</a> - Find me on 
-        <a href='https://www.linkedin.com/in/paclaraujo/'>LinkedIn</a>
+        <a href='https://www.linkedin.com/in/paclaraujo/'> LinkedIn</a>
       </p>
     </section>
   )
