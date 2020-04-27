ae882daee11610be5195da91bdb072ea63dbb1dc
paclaraujo <paclaraujo@gmail.com>
2020-04-27 12:09:39 -0300

    add card component responsive style

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React , { useState } from 'react';
 import Card from '../components/Card';
+import jsonData from '../data/data.json';
 import './style.css';
 
 const Home = () => {
+  const data = jsonData.questions;
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const saveScore = (answer) => {
+    setIndex(index + 1)
+    return answer === data[index].correct_answer ? setScore(score + 1) : false;
+  } 
   
   return (
     <div className='home-container'>
-      <Card/>
+      <Card 
+        question={data[index].question} 
+        answers={data[index].answers} 
+        handleClick={saveScore}
+      />
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Button from '../Button';
 import './style.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <section className='box'>
       <h1 className='card-title'>How well do you know the world?</h1>
-      <div>
-        <Button title='teste'/>
+      <p className='question'>{props.question}</p>
+      <div className='answers-box'>
+        {props.answers.map(answer => 
+          <Button handleClick={() => props.handleClick(answer)} title={answer} />   
+        )}
       </div>
       <p class='contacts'>Developed by 
         <a href='https://github.com/paclaraujo'>@paclaraujo</a> - Find me on 
