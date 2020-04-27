47cfe9de402c900f465b8ff5e4a253b274002253
paclaraujo <paclaraujo@gmail.com>
2020-04-26 21:22:39 -0300

    add card style

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,14 +3,14 @@ import './style.css';
 
 const Card = () => {
   return (
-    <section>
+    <section className='box'>
       <h1>How well do you know the world?</h1>
       <div>
 
       </div>
-      <p class="contact">Developed by 
-        <a href="https://github.com/paclaraujo">@paclaraujo</a> - Find me on 
-        <a href="https://www.linkedin.com/in/paclaraujo/">LinkedIn</a>
+      <p class='contacts'>Developed by 
+        <a href='https://github.com/paclaraujo'>@paclaraujo</a> - Find me on 
+        <a href='https://www.linkedin.com/in/paclaraujo/'>LinkedIn</a>
       </p>
     </section>
   )
